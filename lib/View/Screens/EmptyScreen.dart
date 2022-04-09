d066e5e570e6b5a6561ac79b8d251412bb5b7c13
Naima Hasan <naimahasan31@gmail.com>
2022-04-09 19:06:32 +0600

    Added Launcher icon and edited empty screen images, {has error in AndroidManifest.xml}

--- lib/View/Screens/EmptyScreen.dart
+++ lib/View/Screens/EmptyScreen.dart
@@ -26,7 +26,7 @@ class EmptyScreen extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Image.asset(
-          'Assets/noSearchResult.jpg',
+          'Assets/noSearchResults.jpg',
           fit: BoxFit.cover,
         ),
       ),
