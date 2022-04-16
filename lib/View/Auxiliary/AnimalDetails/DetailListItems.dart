da1b382caa371b96699414b95c8e81ed2fd29087
Naima Hasan <naimahasan31@gmail.com>
2022-04-16 16:12:03 +0600

    Changed image not available image and altered some padding

--- lib/View/Auxiliary/AnimalDetails/DetailListItems.dart
+++ lib/View/Auxiliary/AnimalDetails/DetailListItems.dart
@@ -29,12 +29,16 @@ class DetailListItems extends StatelessWidget {
             ),
           ),
           (list.length == 1 &&
-                  (list[0] == 'Not Available' || (list[0] == 'Not available')))
+                  (list[0] == 'Not Available' ||
+                      (list[0] == 'Not available') ||
+                      (list[0] == 'N/A') ||
+                      (list[0] == 'n/a') ||
+                      (list[0] == 'not available')))
               ? Padding(
                   padding: EdgeInsets.only(top: 16),
                   child: Center(
                     child: Text(
-                      list[0],
+                      'Not Available',
                       style: TextStyle(
                         fontSize: 15,
                       ),
